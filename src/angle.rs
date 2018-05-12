01f40a3f9c1ca6dc7c961b1809f5f282f8d761ca
Mike Dilger <mike@optcomp.nz>
2018-05-12 12:04:27 +1200

    Angle

---
+++ src/angle.rs
@@ -0,0 +1,221 @@
+use FullFloat;
+use cgmath::{Deg, Rad};
+use num_traits::{NumCast, Zero, One};
+use std::ops::{Add, Div, Mul, Sub, Rem, Neg};
+
+/// An angle.
+///
+/// When interoperating with numbers, functions are explicit
+/// about the units of those numbers (radians, degrees or cycles).
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialOrd, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct Angle<F>(F);
+
+impl<F: FullFloat> Angle<F> {
+    /// Create an angle from radians
+    #[inline]
+    pub fn new_radians(radians: F) -> Angle<F> {
+        Angle::<F>::from_radians(radians)
+    }
+
+    /// Create an angle from radians
+    pub fn from_radians(radians: F) -> Angle<F> {
+        Angle(radians)
+    }
+
+    /// Get the value of the angle as radians
+    pub fn as_radians(&self) -> F {
+        self.0
+    }
+
+    /// Create an angle from degrees
+    #[inline]
+    pub fn new_degrees(degrees: F) -> Angle<F> {
+        Angle::<F>::from_degrees(degrees)
+    }
+
+    /// Create an angle from degrees
+    pub fn from_degrees(degrees: F) -> Angle<F> {
+        let one_eighty: F = NumCast::from(180.0_f64).unwrap();
+        Angle(F::PI() * degrees / one_eighty)
+    }
+
+    /// Get the value of the angle as degrees
+    pub fn as_degrees(&self) -> F {
+        let one_eighty: F = NumCast::from(180.0_f64).unwrap();
+        self.0 * one_eighty / F::PI()
+    }
+
+    /// Create an angle from cycles (1 cycle is a full circle)
+    #[inline]
+    pub fn new_cycles(cycles: F) -> Angle<F> {
+        Angle::<F>::from_cycles(cycles)
+    }
+
+    /// Create an angle from cycles (1 cycle is a full circle)
+    pub fn from_cycles(cycles: F) -> Angle<F> {
+        let two: F = NumCast::from(2.0_f64).unwrap();
+        Angle(two * F::PI() * cycles)
+    }
+
+    /// Get the value of the angle as number of cycles (full circles)
+    pub fn as_cycles(&self) -> F {
+        let two: F = NumCast::from(2.0_f64).unwrap();
+        self.0 / (two * F::PI())
+    }
+
+    /// This normalizes to the radian range -PI to PI (half cycle each way)
+    pub fn normalize_around_zero(&mut self) {
+        let two: F = NumCast::from(2.0_f64).unwrap();
+        self.0 = self.0 % (two * F::PI()) - F::PI();
+    }
+
+    /// This normalizes to the radian range 0 to 2*PI (full circle, positive)
+    pub fn normalize_as_positive(&mut self) {
+        let two: F = NumCast::from(2.0_f64).unwrap();
+        self.0 = self.0 % (two * F::PI());
+    }
+}
+
+impl<F: FullFloat> Add<Angle<F>> for Angle<F> {
+    type Output = Angle<F>;
+
+    fn add(self, rhs: Angle<F>) -> Angle<F> {
+        Angle(self.0 + rhs.0)
+    }
+}
+
+impl<F: FullFloat> Sub<Angle<F>> for Angle<F> {
+    type Output = Angle<F>;
+
+    fn sub(self, rhs: Angle<F>) -> Angle<F> {
+        Angle(self.0 - rhs.0)
+    }
+}
+
+impl<F: FullFloat> Mul<Angle<F>> for Angle<F> {
+    type Output = Angle<F>;
+
+    fn mul(self, rhs: Angle<F>) -> Angle<F> {
+        Angle(self.0 * rhs.0)
+    }
+}
+
+impl<F: FullFloat> Div<Angle<F>> for Angle<F> {
+    type Output = Angle<F>;
+
+    fn div(self, rhs: Angle<F>) -> Angle<F> {
+        Angle(self.0 / rhs.0)
+    }
+}
+
+impl<F: FullFloat> Rem<Angle<F>> for Angle<F> {
+    type Output = Angle<F>;
+
+    fn rem(self, rhs: Angle<F>) -> Angle<F> {
+        Angle(self.0 % rhs.0)
+    }
+}
+
+impl<F: FullFloat> Mul<F> for Angle<F> {
+    type Output = Angle<F>;
+
+    fn mul(self, rhs: F) -> Angle<F> {
+        Angle(self.0 * rhs)
+    }
+}
+
+impl<F: FullFloat> Div<F> for Angle<F> {
+    type Output = Angle<F>;
+
+    fn div(self, rhs: F) -> Angle<F> {
+        Angle(self.0 / rhs)
+    }
+}
+
+impl<F: FullFloat> Neg for Angle<F> {
+    type Output = Angle<F>;
+
+    fn neg(self) -> Angle<F> {
+        Angle(-self.0)
+    }
+}
+
+impl<F: FullFloat> Zero for Angle<F> {
+    fn zero() -> Angle<F> {
+        Angle(F::zero())
+    }
+    fn is_zero(&self) -> bool {
+        self.0.is_zero()
+    }
+}
+
+impl<F: FullFloat> One for Angle<F> {
+    fn one() -> Angle<F> {
+        Angle(F::one())
+    }
+    fn is_one(&self) -> bool {
+        self.0.is_one()
+    }
+}
+
+impl<F> From<Rad<F>> for Angle<F> {
+    #[inline]
+    fn from(rad: Rad<F>) -> Angle<F> {
+        Angle(rad.0)
+    }
+}
+
+impl<F> Into<Rad<F>> for Angle<F> {
+    #[inline]
+    fn into(self) -> Rad<F> {
+        Rad(self.0)
+    }
+}
+
+impl<F> From<Deg<F>> for Angle<F>
+where
+    F: FullFloat,
+{
+    #[inline]
+    fn from(deg: Deg<F>) -> Angle<F> {
+        Angle::from_degrees(deg.0)
+    }
+}
+
+impl<F> Into<Deg<F>> for Angle<F>
+where
+    F: FullFloat,
+{
+    #[inline]
+    fn into(self) -> Deg<F> {
+        Deg(self.as_degrees())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_radians() {
+        let f: f32 = 1.234;
+        let a = Angle::from_radians(f);
+        assert_eq!(a.as_radians(), f);
+    }
+
+    #[test]
+    fn test_degrees() {
+        let f: f32 = 1.234;
+        let a = Angle::from_degrees(f);
+        assert_eq!(a.as_degrees(), f);
+    }
+
+    #[test]
+    fn test_cycles() {
+        let f: f32 = 1.234;
+        let a = Angle::from_cycles(f);
+        assert_eq!(a.as_cycles(), f);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ extern crate num_traits;
 use num_traits::{Float, FloatConst, NumAssignOps};
 use std::fmt::Debug;
 
+mod angle;
+pub use self::angle::Angle;
+
 // This trait allows us to write code generic across both floating point types
 pub trait FullFloat: Float + FloatConst + NumAssignOps + Clone + Debug {}
 
